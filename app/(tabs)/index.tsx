3aac4220e543f800600e15939b14f00689945498
Kishan Vyas <kishanvyas308@gmail.com>
2025-06-16 11:12:02 +0530

    search setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,14 @@
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 import '../global.css';
+
 export default function Index() {
+
+  const router = useRouter();
+
   return (
     <View
       className="flex-1 bg-primary "
@@ -18,6 +24,13 @@ export default function Index() {
           source={icons.logo}
           className="w-12 h-10 mt-20 mx-auto"
           />
+
+          <View className="flex-1 mt-5" >
+            <SearchBar 
+              onPress={() => router.push('/search')}
+              placeholder="Search Movies"
+            />
+          </View>
       </ScrollView>
     </View>
   );
