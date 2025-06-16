ea68e66d92a07cb6bb22bc1d10ce2528ce647227
Kishan Vyas <kishanvyas308@gmail.com>
2025-06-16 18:41:30 +0530

    search functionility added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,50 +1,31 @@
 import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { Tabs } from 'expo-router'
-import { useEffect } from 'react'
-import { Image, ImageBackground, Text } from 'react-native'
-import Animated, { interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
+import { Image, ImageBackground, Text, View } from 'react-native'
 
 
 
 const TabIcon = ({ focused, title, icon }: { focused: boolean, title: string, icon: any }) => {
-    const animatedValue = useSharedValue(focused ? 1 : 0) 
+  
 
-        useEffect(() => {
-            animatedValue.value = withTiming(focused ? 1 : 0, {
-                duration: 300
-            })
-        }, [focused])
-
-        const animatedStyle = useAnimatedStyle(() => {
-            const scale = interpolate(animatedValue.value, [0, 1], [0.8, 1])
-            const opacity = interpolate(animatedValue.value, [0, 1], [0.5, 1])
-            return {
-                transform: [{ scale }],
-                opacity
-            }
-        })
-    
+       
 
     if (focused) {
 
         return (
-            <Animated.View style={animatedStyle} >
-
             <ImageBackground source={images.highlight} className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden' >
                 <Image source={icon} tintColor="#151312" className='size-5' />
                 <Text className='text-secondary text-base font-semibold ml-2'>{title}</Text>
             </ImageBackground>
-            </Animated.View>
 
         )
     }
 
     return (
-        <Animated.View style={animatedStyle} className='size-full justify-center items-center mt-4 rounded-full' >
+        <View className='size-full justify-center items-center mt-4 rounded-full' >
 
             <Image source={icon} className='size-5' tintColor="#A8B5DB" />
-        </Animated.View>
+        </View>
 
 
 
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -4,13 +4,34 @@ import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { fetchMovies } from '@/services/api'
 import useFetch from '@/services/useFetch'
-import { useRouter } from 'expo-router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
 
 const Search = () => {
-  const router = useRouter();
-  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: '' }))
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const { data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+    refetch: loadMovies,
+    reset,
+  } = useFetch(() => fetchMovies({
+    query: searchQuery
+  }), false)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+
+      if (searchQuery.trim()) {
+        await loadMovies()
+      } else {
+        reset()
+      }
+    }, 500)
+
+    return () => clearTimeout(timeoutId)
+  }, [searchQuery])
+
   return (
     <View className='flex-1 bg-primary'>
       <Image source={images.bg} className='flex-1 absolute w-full z-0' resizeMode='cover' />
@@ -38,6 +59,8 @@ const Search = () => {
             <View className='my-5'>
               <SearchBar
                 placeholder='Search Movies...'
+                value={searchQuery}
+                onChangeText={(text: any) => setSearchQuery(text)}
               />
             </View>
 
@@ -49,7 +72,7 @@ const Search = () => {
               />
             }
             {
-              moviesError && 
+              moviesError &&
               <View className='my-3'>
                 <Text className='text-red-500 text-center'>
                   Error: {moviesError?.message}
@@ -58,14 +81,28 @@ const Search = () => {
             }
 
             {
-              !moviesLoading && !moviesError && 'SEARCH TERM'.trim() && movies?.length > 0 && (
+              !moviesLoading && !moviesError && searchQuery.trim() && movies?.length > 0 && (
                 <Text className='text-xl text-white font-bold'>
                   Search Results for {" "}
-                  <Text className='text-accent'>SEARCH TERM</Text>
+                  <Text className='text-accent'>{searchQuery.trim()}</Text>
                 </Text>
               )
             }
           </>
+        }
+
+        ListEmptyComponent={
+          !moviesLoading && !moviesError ? (
+            <View className='mt-10 px-5'>
+              <Text className='text-center text-gray-500'>
+                {searchQuery.trim() ? 'No movies found' : 'Search for a movie'}
+
+              </Text>
+              </View>
+          ) : (
+            <>
+            </>
+          )
         }
       />
 
