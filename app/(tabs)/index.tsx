fb8b7f542e302d48187ccaa8d2b60bdbd84e87b6
Kishan Vyas <kishanvyas308@gmail.com>
2025-06-16 10:01:48 +0530

    simple route setup

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import '../global.css';
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center "
+    >
+      <Text className="text-dark-100 text-4xl">Hyyy</Text>
+    </View>
+  );
+}
