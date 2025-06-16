ee2dbef6b371d1a2586f9b0a4ec2f9e6a1ef8006
Kishan Vyas <kishanvyas308@gmail.com>
2025-06-16 17:24:46 +0530

    home page design complete

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
+import { icons } from '@/constants/icons'
 import { Link } from 'expo-router'
 import React from 'react'
-import { Image, Text, TouchableOpacity } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 
 const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie) => {
     return (
@@ -16,9 +17,22 @@ const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie
                     resizeMode='cover'
                 />
 
-                <Text className='text-white text-sm font-bold mt-2'>
+                <Text className='text-white text-sm font-bold mt-2' numberOfLines={1}>
                     {title}
                 </Text>
+
+                <View className='flex-row item-center justify-start gap-x-1'>
+                    <Image source={icons.star} className='size-4' />
+                    <Text className='text-xs text-white font-bold uppercase'>{(vote_average).toFixed(1)}</Text>
+                </View>
+                
+
+                <View className='flex-row items-center justify-between'>
+                    <Text className='text-xs text-light-300 font-medium mt-1'>
+                        {release_date?.split('-')[0]}
+                    </Text>
+                   
+                </View>
 
 
             </TouchableOpacity>
